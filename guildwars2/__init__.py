f999efc43c314e4b5759a8291182bc1870366d90
Jason Straw <jason.straw@gmail.com>
2013-05-23 12:56:43 -0500

    Add setup.py to allow easy_install/pip to work.
    Add includes to ignore list

--- guildwars2/__init__.py
+++ guildwars2/__init__.py
@@ -10,29 +10,29 @@ import maps
 #import recipes
 
 
+version = '0.0.0'
 
 class GuildWars2:
-   base_url = 'https://api.guildwars2.com/v1/'
-   ver = '0.0.0'
-   user_agent = 'python-guildwars2 %s' % ver
+    base_url = 'https://api.guildwars2.com/v1/'
+    user_agent = 'python-guildwars2 %s' % version
 
-   def __init__(self, lang='en', user_agent=None):
-      self.lang = lang
-      if 'GW2_DEBUG' in os.environ:
-         self.debug = True
-      if user_agent:
-         self.user_agent = user_agent
-      self.maps = maps.maps(self)
-      self.worlds = worlds.worlds(self)
+    def __init__(self, lang='en', user_agent=None):
+        self.lang = lang
+        if 'GW2_DEBUG' in os.environ:
+            self.debug = True
+        if user_agent:
+            self.user_agent = user_agent
+        self.maps = maps.maps(self)
+        self.worlds = worlds.worlds(self)
 
 
-   
-   def _request(self, uri, **kwargs):
-      url = '%s%s.json' % (self.base_url, uri)
-      kwargs.setdefault('headers', {})['Accept'] = 'application/json'
-      kwargs['headers']['User-Agent'] = self.user_agent
+    
+    def _request(self, uri, **kwargs):
+        url = '%s%s.json' % (self.base_url, uri)
+        kwargs.setdefault('headers', {})['Accept'] = 'application/json'
+        kwargs['headers']['User-Agent'] = self.user_agent
 
-      kwargs.setdefault('params', None)
-      
-      r = requests.get(url, params=kwargs['params'], headers=kwargs['headers'])
-      return r.json()
+        kwargs.setdefault('params', None)
+        
+        r = requests.get(url, params=kwargs['params'], headers=kwargs['headers'])
+        return r.json()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from guildwars2 import version
+
+
+install_requires = [
+    'requests'
+]
+
+setup(name='python-guildwars2',
+    version=version,
+    description="Guild Wars 2 Python Binding",
+    author='Jason Straw',
+    author_email='jason.straw@gmail.com',
+    url='http://github.com/jstraw/python-guildwars2/',
+    install_requires=install_requires,
+)
